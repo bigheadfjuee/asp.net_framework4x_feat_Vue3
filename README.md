206a26418b2f4bf9b2fd9a83be41670875188c52
Tony <bigheadfjuee@gmail.com>
2023-07-08 16:58:23 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # asp.net_framework4x_feat_Vue3
 ASP.NET Framework 4.x feat Vue 3 (bundle)
+
+## MVC
+
+BundleConfig.cs 加入 Vue3 的 bundle
+
+Devmo 在 Views\Vue.cshtml 裡面使用 Vue3
+
+## Vue3 
+
+Scripts\vue.global.js
+
+Scripts\vue3-sfc-loader.js
+
+Scripts\vue-myComponent.js
+  
---
+++ WebApplication1/Scripts/myComponent.js
@@ -0,0 +1,24 @@
+﻿// dotNet MVC 不能使用 .vue 文件，所以使用 .js 文件代替
+// 放在 cshtml 會導致排版完全失效
+const componentSource = /* <!-- */`
+      <template>
+        <span class="example">{{ msg }}</span>
+      </template>
+      <script>
+        export default {
+          data () {
+            return {
+              msg: 'world!'
+            }
+          }
+        }
+      </script>
+
+      <style scoped>
+        .example {
+          color: red;
+        }
+      </style>
+    `/* --> */;
+
+export { componentSource };
